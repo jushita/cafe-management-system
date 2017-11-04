fb009082969e447d05fe287c3dff80975b52d7ac
Jushita Rahman <jushitaa@gmail.com>
2017-11-04 12:24:50 -0400

    initial frames added

--- program.py
+++ program.py
@@ -5,8 +5,17 @@ import datetime
 
 
 root = Tk()
-root.geometry("1350x750+0+0")
+root.geometry("1350x50+0+0")
 root.title("Cafe Management Systems")
-root.configure(background="black")
+root.configure(background = "black")
+
+Tops = Frame(root, width = 1350, height = 100, bd = 14, relief = "raise")
+Tops.pack(side = TOP)
+
+f1 = Frame(root, width = 900, height = 650, bd = 8, relief = "raise")
+f1.pack(side = LEFT)
+
+f2 = Frame(root, width = 440, height = 650, bd = 8, relief = "raise")
+f2.pack(side = RIGHT)
 
 root.mainloop()
