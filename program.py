d97e7c6ec70b6b1732da784bb73820ec0afbc71a
Jushita Rahman <jushitaa@gmail.com>
2017-11-04 11:49:54 -0400

    initial commit

---
+++ program.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import random
+import time
+import datetime
+
+
+root = Tk()
+root.geometry("1350x750+0+0")
+root.title("Cafe Management Systems")
+root.configure(background="black")
+
+root.mainloop()
