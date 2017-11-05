0d0041ec747f23e36523b96456b4aa036dfb1eee
Jushita Rahman <jushitaa@gmail.com>
2017-11-04 20:45:43 -0400

    title added

--- program.py
+++ program.py
@@ -18,4 +18,35 @@ f1.pack(side = LEFT)
 f2 = Frame(root, width = 440, height = 650, bd = 8, relief = "raise")
 f2.pack(side = RIGHT)
 
+
+f1a = Frame(f1, width = 90, height = 330, bd = 8, relief = "raise")
+f1a.pack(side = TOP)
+
+f2a = Frame(f1, width = 90, height = 320, bd = 6, relief = "raise")
+f2a.pack(side = BOTTOM)
+
+ft2 = Frame(f2, width = 440, height = 650, bd = 12, relief = "raise")
+ft2.pack(side = TOP)
+fb2 = Frame(f2, width = 440, height = 330, bd = 16, relief = "raise")
+fb2.pack(side = BOTTOM)
+
+f1aa = Frame(f1a, width = 400, height = 330, bd = 16, relief = "raise")
+f1aa.pack(side = LEFT)
+f1ab = Frame(f1a, width = 400, height = 330, bd = 16, relief = "raise")
+f1ab.pack(side = RIGHT)
+
+f2aa = Frame(f2a, width = 450, height = 330, bd = 14, relief = "raise")
+f2aa.pack(side = LEFT)
+
+f2ab = Frame(f2a, width = 450, height = 330, bd = 6, relief = "raise")
+f2ab.pack(side = RIGHT)
+
+
+Tops.configure (background = 'black')
+f1.configure (background = 'black')
+f2.configure (background = 'black')
+
+lblInfo = Label(Tops, font  = ('arial', 70, 'bold'), text = "Cafe Management Systems", bd = 10)
+lblInfo.grid(row = 0, column = 0)
+
 root.mainloop()
